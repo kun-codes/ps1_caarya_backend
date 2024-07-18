8ce7f14246543e2da1ff6b1e3f1a1e801b963a30
kun-codes <77796630+throwaway69420-69420@users.noreply.github.com>
2024-07-19 00:05:21 +0530

    Implement register as employer

--- market/forms.py
+++ market/forms.py
@@ -24,6 +24,23 @@ class RegisterPlayerForm(FlaskForm):
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
+class RegisterEmployerForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Company Email Address:', validators=[Email(), DataRequired()])
+    valorant_username = StringField(label='Valorant Username:', validators=[Length(min=2, max=30), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterPlayerForm, LoginForm
+from market.forms import RegisterPlayerForm, LoginForm, RegisterEmployerForm
 from market import db
 from flask_login import login_user, logout_user, login_required
 
@@ -39,6 +39,26 @@ def register_player():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register-player.html', form=form)
+
+@app.route('/register-employer', methods=['GET', 'POST'])
+def register_employer():
+    form = RegisterEmployerForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              valorant_username=form.valorant_username.data,
+                              password=form.password1.data,
+                              user_type='employer')
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register-employer.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
